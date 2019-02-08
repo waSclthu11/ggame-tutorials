df1381fe367578f123f0d860393e9ab7d0a290e1
waSclthu11 <46973645+waSclthu11@users.noreply.github.com>
2019-02-08 14:55:48 -0500

    Updated from Brython Server: 2/8/2019 2:55:46 PM

--- tutorial3.py
+++ tutorial3.py
@@ -1,1 +1,13 @@
 """waSclthu11's ggame tutorial"""
+from ggame import App, RectangleAsset, ImageAsset, SoundAsset
+from ggame import LineStyle, Color, Sprite, Sound
+
+myapp = App()
+myapp.run()
+# define colors and line style
+green = Color(0x00ff00, 1)
+black = Color(0, 1)
+noline = LineStyle(0, black)
+# a rectangle asset and sprite to use as background
+bg_asset = RectangleAsset(myapp.width, myapp.height, noline, green)
+bg = Sprite(bg_asset, (0,0))

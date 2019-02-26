edf61ba088e2253882227d2558d63351d89f97ae
waSclthu11 <46973645+waSclthu11@users.noreply.github.com>
2019-02-26 14:34:09 -0500

    Updated from Brython Server: 2/26/2019 2:34:07 PM

--- tutorial4.py
+++ tutorial4.py
@@ -5,7 +5,7 @@ class SpaceGame(App):
     Tutorial4 space game example.
     """
     def __init__(self):
-        super().__init__()yapp = App()
+        super().__init__()
         black = Color(0, 1)
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(myapp.width, myapp.height, noline, black)

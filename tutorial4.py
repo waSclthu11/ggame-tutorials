6b3112cf882a54412d810d0de22e1651c4b3ea29
waSclthu11 <46973645+waSclthu11@users.noreply.github.com>
2019-02-26 14:34:47 -0500

    Updated from Brython Server: 2/26/2019 2:34:46 PM

--- tutorial4.py
+++ tutorial4.py
@@ -6,11 +6,11 @@ class SpaceGame(App):
     """
     def __init__(self):
         super().__init__()
+        # Background
         black = Color(0, 1)
         noline = LineStyle(0, black)
-        bg_asset = RectangleAsset(myapp.width, myapp.height, noline, black)
+        bg_asset = RectangleAsset(self.width, self.height, noline, black)
         bg = Sprite(bg_asset, (0,0))
-# Background
 
 myapp=Spacegame()
 myapp.run()
